c602bb5735f54514eb27bfb65811271ee6872d1a
Thrashtaker <edgars.spengelis@gmail.com>
2018-11-16 19:44:37 +0200

    add a paragraph to page 1

--- components/page1.php
+++ components/page1.php
@@ -1,4 +1,5 @@
 <h1>This is Sparta!</h1>
+<p>Indeed it is</p>
 <div>
 <a href=""><img src="https://img.huffingtonpost.com/asset/58c05efa1d000037037cd491.jpeg?ops=scalefit_720_noupscale"  /></a>;
 </div>
